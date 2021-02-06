38af91ea74d501d3e32c5c1707d5674ee6b47dd1
Sophia Brandt <git@s-br.net>
2021-02-06 11:42:10 +0100

    Clean-up css, fix error message typo

--- internal/data/errorMessages.go
+++ internal/data/errorMessages.go
@@ -9,7 +9,7 @@ import (
 // from the validation package with more human-friendly messages.
 func TranslateOverride(validate *validator.Validate, trans ut.Translator) {
 	validate.RegisterTranslation("required_without", trans, func(ut ut.Translator) error {
-		return ut.Add("required_without", "Must search for at least 1 or 1 Game Master", true)
+		return ut.Add("required_without", "At least 1 Player or 1 Game Master required!", true)
 	}, func(ut ut.Translator, fe validator.FieldError) string {
 		t, _ := ut.T("required_without", fe.Field())
 
